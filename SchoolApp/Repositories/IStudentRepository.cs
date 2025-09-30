21b81bf3fbec5e298d16ed1714c55730fabecf62
Vpetros <rawkudodes@hotmail.com>
2025-09-30 20:59:38 +0300

    feat: add interface student repository

---
+++ SchoolApp/Repositories/IStudentRepository.cs
@@ -0,0 +1,15 @@
+﻿using SchoolApp.Data;
+using SchoolApp.Models;
+using System.Linq.Expressions;
+
+namespace SchoolApp.Repositories
+{
+    public interface IStudentRepository
+    {
+        Task<List<Course>> GetStudentCoursesAsync(int studentId);
+        Task<Student?> GetByAm(string? am);
+        Task<PaginatedResult<Student>> GetPaginatedUsersStudentsAsync();
+        Task<PaginatedResult<Student>> GetPaginatedUsersStudentsFilteredAsync(int pageNumber, int pageSize,
+            List<Expression<Func<User, bool>>> predicates);
+    }
+}
