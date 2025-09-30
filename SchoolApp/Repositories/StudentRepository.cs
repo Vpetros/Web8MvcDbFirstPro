f7ff48c58eb785e5063a50b59347e082434d8484
Vpetros <rawkudodes@hotmail.com>
2025-09-30 21:00:05 +0300

    feat: add student repository

---
+++ SchoolApp/Repositories/StudentRepository.cs
@@ -0,0 +1,33 @@
+﻿using SchoolApp.Data;
+using SchoolApp.Models;
+using System.Linq.Expressions;
+
+namespace SchoolApp.Repositories
+{
+    public class StudentRepository : BaseRepository<Student>, IStudentRepository
+    {
+        public StudentRepository(Mvc8DbProContext context) : base(context)
+        {
+        }
+
+        public Task<Student?> GetByAm(string? am)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PaginatedResult<Student>> GetPaginatedUsersStudentsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PaginatedResult<Student>> GetPaginatedUsersStudentsFilteredAsync(int pageNumber, int pageSize, List<Expression<Func<User, bool>>> predicates)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Course>> GetStudentCoursesAsync(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
