362e435ca604833a4cf095e5f0e6d5afc6d8c5e0
Vpetros <rawkudodes@hotmail.com>
2025-09-30 20:52:20 +0300

    feat: add unit of work

---
+++ SchoolApp/Repositories/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using SchoolApp.Data;
+
+namespace SchoolApp.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly Mvc8DbProContext context;
+
+        public UnitOfWork(Mvc8DbProContext context)
+        {
+            this.context = context;
+        }
+
+
+        public UserRepository UserRepository => new(context);
+        
+        public TeacherRepository TeacherRepository => new(context);
+
+        
+        public async Task<bool> SaveAsync()
+        {
+            return await context.SaveChangesAsync() > 0;    // κάνει commit και αυτόματα rollback αν αποτύχει
+        }
+    }
+}
