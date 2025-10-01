b5e7ca7285b151a47d8b79974cf46ae4117a0186
Vpetros <rawkudodes@hotmail.com>
2025-10-01 21:48:09 +0300

    feat: add appsettings and program services

--- SchoolApp/Program.cs
+++ SchoolApp/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using SchoolApp.Data;
 using SchoolApp.Repositories;
+using SchoolApp.Services;
+using Serilog;
 
 namespace SchoolApp
 {
@@ -10,13 +14,29 @@ namespace SchoolApp
             var builder = WebApplication.CreateBuilder(args);
 
             var connString = builder.Configuration.GetConnectionString("DefaultConnection");
-            builder.Services.AddDbContext<Data.Mvc8DbProContext>(options =>
-                options.UseSqlServer(connString));
+            connString = connString!.Replace("{DB_PASS}", Environment.GetEnvironmentVariable("DB_PASS") ?? "");
 
-            builder.Services.AddRepositories(); // extension method from RepositoriesDIExtensions
+
+            builder.Services.AddDbContext<Mvc8DbProContext>(options => options.UseSqlServer(connString));
+            builder.Services.AddRepositories();
+            builder.Services.AddScoped<IApplicationService, ApplicationService>();
+
+            builder.Services.AddAutoMapper(cfg => cfg.AddProfile<Configuration.MapperConfig>());
+            builder.Host.UseSerilog((context, config) =>
+            {
+                config.ReadFrom.Configuration(context.Configuration);
+            });
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/User/Login";
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                    options.SlidingExpiration = true;   // reset timeout, 30 min of idle
+                });
 
             var app = builder.Build();
 
@@ -33,11 +53,12 @@ namespace SchoolApp
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=User}/{action=Login}/{id?}");
 
             app.Run();
         }
