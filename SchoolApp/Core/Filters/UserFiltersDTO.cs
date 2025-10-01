400cf521194e1d5163a0b62f43ce9bd4a4beaae9
Vpetros <rawkudodes@hotmail.com>
2025-10-01 21:44:27 +0300

    feat: add filters

---
+++ SchoolApp/Core/Filters/UserFiltersDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolApp.Core.Filters
+{
+    public class UserFiltersDTO
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? UserRole { get; set; }
+    
+    }
+}
