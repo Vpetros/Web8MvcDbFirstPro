93000127c357b46d9076980e35d23bab037fab6f
Vpetros <rawkudodes@hotmail.com>
2025-10-01 21:45:29 +0300

    feat: add services

---
+++ SchoolApp/Services/IApplicationService.cs
@@ -0,0 +1,8 @@
+﻿namespace SchoolApp.Services
+{
+    public interface IApplicationService
+    {
+        UserService UserService { get; }
+        TeacherService TeacherService { get; }
+        }
+}
